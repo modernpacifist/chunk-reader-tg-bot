331ae51d62d91fb7d48a51f84181bc3dd45450b5
modernpacifist <modernpacifist@gmail.com>
2022-10-04 18:50:57 +0300

    solve problem with encoding from here

--- src/PdfManager.py
+++ src/PdfManager.py
@@ -1,12 +1,14 @@
 import PyPDF2
+import codecs
  
 #create file object variable
 #opening method will be rb
 
 
 if __name__ == "__main__":
+    sampleFileName = "C:\\Users\\vp\\Documents\\books\\Dive_Into_Design_Patterns.pdf"
     try:
-        pdffileobj = open('1.pdf', 'rb')
+        pdffileobj = open(sampleFileName, "rb")
     except Exception as e:
         print(e)
 
@@ -15,8 +17,14 @@ if __name__ == "__main__":
     
     for i in range(total_pages_quantity):
         pageobj = pdfreader.getPage(i)
-        text = pageobj.extractText()
+        try:
+            text = pageobj.extractText().encode("utf-8")
+        except Exception as e:
+            print(e)
+            continue
 
-        file1 = open(r"result_text.txt", "a")
-        file1.writelines(text + '\n')
+        # TODO: sovle problem with artifact chars and encoding
+        with codecs.open("result_text.txt", "a", "utf-8-sig") as result_file:
+            result_file.write(str(text) + '\n')
+
         print(text)
