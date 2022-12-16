0b5258adeb8646e9115c845203d880d438ad3e65
modernpacifist <modernpacifist@gmail.com>
2022-12-16 17:59:51 +0300

    mybooks command:minor refactoring

--- src/main.py
+++ src/main.py
@@ -277,16 +277,14 @@ def mybooks(update, context) -> None:
     # this is duplicated
     mongo_db_cursor = mongodbmanager.get_user_books(uid)
     owner_books = [item for item in mongo_db_cursor]
-    # owner_books = {item['index']:item for item in mongo_db_cursor}
 
     shared_db_cursor = mongodbmanager.get_shared_books()
     shared_books = [item for item in shared_db_cursor]
-    # shared_books = {item['index']:item for item in shared_db_cursor}
 
+    user_books = ""
+    shared_books_message = ""
     currently_reading_string = ""
     files_list_message = ""
-    shared_books_message = ""
-    user_books = ""
 
     current_book = None
     # TODO: use builder pattern here <14-12-22, modernpacifist> #
@@ -296,6 +294,9 @@ def mybooks(update, context) -> None:
             book_index = book.get('index')
             book_shared = book.get('shared')
 
+            if None in [book_title, book_index, book_shared]:
+                continue
+
             user_books += f"{i}: Title: \"{book_title}\" Index: {book_index} Shared: {book_shared}\n"
 
             book_read_progress = user.get_book_progress(book)
@@ -304,46 +305,42 @@ def mybooks(update, context) -> None:
                 user_books = user_books[:-1] + f" Completion: {book_read_progress}%\n"
 
             # double check this code later
-            if user.current_read_target == book.get('index'):
-                # BUG: currently_reading is possibly unbound
-                # book_read_progress = user.get_book_progress(book)
-                # currently_reading_string = f"Currently reading:\nTitle: \"{book.get('title')}\" Index: {book.get('index')}\n\n"
+            if user.current_read_target == book_index:
                 current_book = book
 
         for i, book in enumerate(shared_books, start=1):
-            # TODO: error with shared books getting name
-            if book.get('title') is None:
+            book_title = book.get('title')
+            book_index = book.get('index')
+
+            if None in [book_title, book_index]:
                 continue
 
-            shared_books_message += f"{i}: Title: \"{book.get('title')}\" Index: {book.get('index')}\n"
+            shared_books_message += f"{i}: Title: \"{book_title}\" Index: {book_index}\n"
 
             book_read_progress = user.get_book_progress(book)
             if book_read_progress is not None:
                 # slice removes \n at the end of default line
                 shared_books_message = shared_books_message[:-1] + f" Completion: {book_read_progress}%\n"
 
-            if user.current_read_target == book.get('index'):
-                # BUG: currently_reading is possibly unbound
-                # book_read_progress = user.get_book_progress(book)
-                # currently_reading_string = f"Currently reading:\nTitle: \"{book.get('title')}\" Index: {book.get('index')}\n\n"
+            if user.current_read_target == book_index:
                 current_book = book
 
-        if current_book:
+        if current_book is not None:
             # BUG: currently_reading is possibly unbound
-            currently_reading_string = f"Currently reading:\nTitle: \"{current_book.get('title')}\" Index: {current_book.get('index')}\n"
+            currently_reading_string = f"Currently reading:\nTitle: \"{current_book.get('title')}\" Index: {current_book.get('index')}\n\n"
             book_read_progress = user.get_book_progress(current_book)
             if book_read_progress is not None:
                 # slice removes \n at the end of default line
-                currently_reading_string = currently_reading_string[:-1] + f" Completion: {book_read_progress}%\n"
+                currently_reading_string = currently_reading_string[:-2] + f" Completion: {book_read_progress}%\n\n"
 
         if user_books != "":
-            files_list_message = files_list_message + f"Your books:\n{user_books}\n"
+            files_list_message += f"Your books:\n{user_books}\n"
 
         if shared_books_message != "":
-            files_list_message = files_list_message + f"Shared books:\n{shared_books_message}\n"
+            files_list_message += f"Shared books:\n{shared_books_message}\n"
 
         if files_list_message != "":
-            update.message.reply_text(f"{currently_reading_string}\n{files_list_message}")
+            update.message.reply_text(f"{currently_reading_string}{files_list_message}")
             return
 
         update.message.reply_text("You have not uploaded any books yet")
