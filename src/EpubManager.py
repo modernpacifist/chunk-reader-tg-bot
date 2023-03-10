17a493b89fda4eb42d1806a9fe7201c65275b6ab
modernpacifist <modernpacifist@gmail.com>
2023-03-10 15:14:59 +0300

    updated epub to txt translation

--- src/EpubManager.py
+++ src/EpubManager.py
@@ -3,35 +3,59 @@ import logging
 
 from ebooklib import epub
 from bs4 import BeautifulSoup
-from cleantext import clean
-# from epub2txt import epub2txt
 
 LOGGER = logging.getLogger(__name__)
 
+EPUB_TAG_BLACKLIST = [
+    '[document]',
+    'noscript',
+    'header',
+    'html',
+    'meta',
+    'head',
+    'input',
+    'script',
+    ]
 
-def to_text(html_item):
-    soup = BeautifulSoup(html_item.get_body_content(), 'html.parser')
-    text = [para.get_text() for para in soup.find_all('p')]
-    return ' '.join(text)
+
+def epub2html(epub_path):
+    book = epub.read_epub(epub_path)
+    chapters = []
+    for item in book.get_items():
+        if item.get_type() == ebooklib.ITEM_DOCUMENT:
+            chapters.append(item.get_content())
+    return chapters
+
+
+def html2ttext(thtml):
+    Output = []
+    for html in thtml:
+        text = chap2text(html)
+        Output.append(text)
+    return Output
+
+
+def chap2text(chap):
+    output = ''
+    soup = BeautifulSoup(chap, 'html.parser')
+    text = soup.find_all(text=True)
+    for t in text:
+        if t.parent.name not in EPUB_TAG_BLACKLIST:
+            output += '{} '.format(t)
+    return output
+
+
+def epub2text(epub_path):
+    chapters = epub2html(epub_path)
+    ttext = html2ttext(chapters)
+    return ttext
 
 
 class EpubManager:
     @staticmethod
     def translateEpubToTxt(file):
-        # try:
-            # return epub2txt(file)
-
-        # except Exception as e:
-            # print(e)
-            # book = epub.read_epub(file)
-            # items = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
-            # raw_text = " ".join([to_text(item) for item in items])
-            # return raw_text.replace("\n", " ")
         try:
-            book = epub.read_epub(file)
-            items = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
-            raw_text = " ".join([to_text(item) for item in items])
-            return raw_text.replace("\n", " ")
+            return " ".join(epub2text(file))
 
         except Exception as e:
             LOGGER.error(e)
